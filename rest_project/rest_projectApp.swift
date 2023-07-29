9377ed9d80221678e1a30ff12c702a2d31500dfd
jke <constantinekulak@proton.me>
2023-07-29 21:48:14 +0300

    Initial Commit

---
+++ rest_project/rest_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  rest_projectApp.swift
+//  rest_project
+//
+//  Created by Constantine Kulak on 29.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct rest_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
